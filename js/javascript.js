3c813c74465a9ed525fbd4dcf16fb93add72338d
Pablo Tovar <pablo.tovar@rgov.co>
2016-12-19 17:33:18 -0600

    Arreglo de problema en javascript

--- js/javascript.js
+++ js/javascript.js
@@ -2,5 +2,5 @@ function init(){
 	console.log("javascript leído");
 	console.log("En esta rama se modificará únicamente el javascript");
 
-	$("body").append("<p class="mensaje">Este es un mensaje agregado vía JavaScript</p>");
+	$("body").append("<p class='mensaje'>Este es un mensaje agregado vía JavaScript</p>");
 }
